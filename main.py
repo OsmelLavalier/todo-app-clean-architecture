2de8f407db2aa0502bdba92bf393aa3911d51252
Osmel Lavalier <osla1802@student.miun.se>
2022-07-22 18:15:53 +0200

    OL: run migration before main app

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import sys
+import subprocess
 from fastapi import FastAPI
 
-from models.base import init
 from routers.v1.todo_router import todo_router
 from routers.v1.user_router import user_router
 
-init()
+try:
+    print("Applying latest migration...")
+    process = subprocess.run(
+        "alembic upgrade head",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    if process.returncode != 0:
+        raise Exception("Unable to run latest migrations...")
+except:
+    sys.exit(1)
+
 
 app = FastAPI(
     title="Todo Backend API",
