af7319e8df5bc0ebdce256fb56314b63cd5f19a0
Osmel Lavalier <osla1802@student.miun.se>
2022-07-22 18:16:09 +0200

    OL: removing base

--- models/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy_utils import create_database, database_exists
-
-from configs.base import DATABASE_URL, engine, Base
-
-
-def init():
-    if not database_exists(DATABASE_URL):
-        create_database(DATABASE_URL)
-        Base.metadata.create_all(bind=engine)
